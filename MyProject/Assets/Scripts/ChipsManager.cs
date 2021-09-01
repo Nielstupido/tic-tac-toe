03929cb6ea85bd44412085bfe47c93773083c1a4
Gairus Niel <nielstupidity@gmail.com>
2021-09-01 16:59:34 +0800

    Added ChipsManager script

---
+++ MyProject/Assets/Scripts/ChipsManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChipsManager : MonoBehaviour
+{
+    [SerializeField]private GameObject[] placeHolders;
+    
+}
