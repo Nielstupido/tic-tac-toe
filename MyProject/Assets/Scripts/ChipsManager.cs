cf5bac5bb6e2e2670ec0b4fe2b5381ead55ec85b
Gairus Niel <nielstupidity@gmail.com>
2021-09-01 22:53:26 +0800

    Player Turn system on scripts
    Added conditions on scripts for the 2 player turn system.

--- MyProject/Assets/Scripts/ChipsManager.cs
+++ MyProject/Assets/Scripts/ChipsManager.cs
@@ -6,13 +6,21 @@ public class ChipsManager : MonoBehaviour
 {
     [SerializeField]private GameObject[] placeHolders;
     [SerializeField]private List<GameObject> p1chips;
+    [SerializeField]private List<GameObject> p2chips;
     private Transform chipTransform;
     private float smoothing = 5f;
     private Vector3 targetPos;
+    private bool p1turn = true;
+
+    public bool P1turn { set {p1turn = value;} }
     
     public void MoveChipTo(int btnNum)
     {
-        chipTransform = p1chips[0].transform;
+        if (p1turn)
+            chipTransform = p1chips[0].transform;
+        else
+            chipTransform = p2chips[0].transform;
+
         targetPos = placeHolders[btnNum-1].transform.position;
 
         StartCoroutine("MoveToTargetPos");
@@ -25,5 +33,10 @@ public class ChipsManager : MonoBehaviour
             chipTransform.position = Vector3.Lerp(chipTransform.position, targetPos, smoothing * Time.deltaTime);
             yield return null;
         }
+
+        if (p1turn)
+            p1chips.RemoveAt(0);
+        else
+            p1chips.RemoveAt(0);
     }
 }
