ce95607de65dfac19faee0e4a321a45bd87e31b9
Gairus Niel <nielstupidity@gmail.com>
2021-09-02 00:19:55 +0800

    Revert "Added a PlayerTurnManager script"
    This reverts commit 4f57e2c68fb3b372df1a4281a923cbe495126ecf.

--- MyProject/Assets/Scripts/ButtonManager.cs
+++ MyProject/Assets/Scripts/ButtonManager.cs
@@ -6,19 +6,16 @@ public class ButtonManager : MonoBehaviour
 {
     private int buttonNum;
     private ChipsManager chipsManager;
-    private PlayerTurnManager playerTurnManager;
 
     void Start()
     {
         chipsManager = FindObjectOfType<ChipsManager>();
-        playerTurnManager = GetComponent<PlayerTurnManager>();
     }
 
     public void PassButtonDets()
     {
         buttonNum = Convert.ToInt32(EventSystem.current.currentSelectedGameObject.name);
         EventSystem.current.currentSelectedGameObject.gameObject.SetActive(false);
-        playerTurnManager.SwitchPlayerTurn();
         chipsManager.MoveChipTo(buttonNum);
     }
 }
