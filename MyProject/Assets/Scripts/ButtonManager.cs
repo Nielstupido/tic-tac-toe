cc8fb4847865349f5df1ea77c5bf11b05344b5a1
Gairus Niel <nielstupidity@gmail.com>
2021-09-01 22:12:22 +0800

    Chips Movement
    Chips movement to target position/placeholder added.

--- MyProject/Assets/Scripts/ButtonManager.cs
+++ MyProject/Assets/Scripts/ButtonManager.cs
@@ -5,10 +5,17 @@ using UnityEngine.EventSystems;
 public class ButtonManager : MonoBehaviour
 {
     private int buttonNum;
+    private ChipsManager chipsManager;
 
-    void PassButtonDets()
+    void Start()
+    {
+        chipsManager = FindObjectOfType<ChipsManager>();
+    }
+
+    public void PassButtonDets()
     {
         buttonNum = Convert.ToInt32(EventSystem.current.currentSelectedGameObject.name);
         EventSystem.current.currentSelectedGameObject.gameObject.SetActive(false);
+        chipsManager.MoveChipTo(buttonNum);
     }
 }
--- MyProject/Assets/Scripts/ChipsManager.cs
+++ MyProject/Assets/Scripts/ChipsManager.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class ChipsManager : MonoBehaviour
 {
     [SerializeField]private GameObject[] placeHolders;
+    [SerializeField]private List<GameObject> p1chips;
+    private Transform chipTransform;
+    private float smoothing = 5f;
+    private Vector3 targetPos;
     
-    void MoveChipTo(int btnNum)
+    public void MoveChipTo(int btnNum)
     {
-        
+        chipTransform = p1chips[0].transform;
+        targetPos = placeHolders[btnNum-1].transform.position;
+
+        StartCoroutine("MoveToTargetPos");
+    }   
+
+    IEnumerator MoveToTargetPos()
+    {
+        while (Vector3.Distance(chipTransform.position, targetPos) > 0.05f)
+        {
+            chipTransform.position = Vector3.Lerp(chipTransform.position, targetPos, smoothing * Time.deltaTime);
+            yield return null;
+        }
     }
 }
